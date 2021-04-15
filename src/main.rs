25bb6d485cdfe35a91f9448549c0c41c26c094fc
ech0r <jrcoop34@gmail.com>
2021-04-14 23:06:21 -0700

    solved problem 12

--- src/main.rs
+++ src/main.rs
@@ -263,6 +263,7 @@ fn problem10() {
     println!("Problem 10: {}", sum_of_primes);
 }
 
+// https://projecteuler.net/problem=11
 fn problem11() {
     let grid_str = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
     let mut grid: [[i32; 20]; 20] = [[0; 20];20];
@@ -279,7 +280,7 @@ fn problem11() {
         }
     }
     let mut largest: i32 = 0;
-    // check horizontal direction
+    // check horizontal and vertical direction
     for y in 0..20 {
         for x in 0..20 {
             if x + 3 > 19 {
@@ -296,7 +297,7 @@ fn problem11() {
         }
     }
     // check diagonal
-    for mut x in 3..20 {
+    for x in 3..20 {
         let mut diagonal_for1: Vec<i32> = Vec::new(); 
         let mut diagonal_for2: Vec<i32> = Vec::new();
         let mut diagonal_bak1: Vec<i32> = Vec::new();
@@ -307,14 +308,86 @@ fn problem11() {
             diagonal_bak1.push(grid[y][19-x+y]);
             diagonal_bak2.push(grid[19-x+y][y]);
         }
-        // get sliding window of 4.
-        let for1 = diagonal_for1.windows(4);
-        let for2 = diagonal_for2.windows(4);
-        let diag1 = diagonal_bak1.windows(4);
-        let diag2 = diagonal_bak2.windows(4);
-        //for1.product()
-
+        // get sliding window of 4, pretty cool! :)
+        let dfor1 = diagonal_for1.windows(4);
+        let dfor2 = diagonal_for2.windows(4);
+        let dbak1 = diagonal_bak1.windows(4);
+        let dbak2 = diagonal_bak2.windows(4);
+        // multiply each segment of 4
+        // These multiplications could be done inside the above loop, but I was checking out
+        // windows in Rust :)
+        for n in dfor1 {
+            let product_df1 = n.iter().product::<i32>();           
+            if product_df1 > largest {
+                largest = product_df1;
+            }
+        }
+        for n in dfor2 {
+            let product_df2 = n.iter().product::<i32>();           
+            if product_df2 > largest {
+                largest = product_df2;
+            }
+        }
+        for n in dbak1 {
+            let product_db1 = n.iter().product::<i32>();           
+            if product_db1 > largest {
+                largest = product_db1;
+            }
+        }
+        for n in dbak2 {
+            let product_db2 = n.iter().product::<i32>();           
+            if product_db2 > largest {
+                largest = product_db2;
+            }
+        }
     }
+    println!("Problem 11: {}", largest);
+}
+
+// https://projecteuler.net/problem=12
+fn problem12() {
+    fn prime_factorizer(mut n: i32, mut p: i32, mut factors: Vec<i32>) -> Vec<i32> {
+        if n < 2 {
+            return factors;
+        }
+        if n % p == 0 {
+            n = n/p;
+            factors.push(p);
+            return prime_factorizer(n, p, factors);
+        } else {
+            p += 1;
+            return prime_factorizer(n, p, factors);
+        }
+    }
+    let mut n = 3;
+    let mut t = 3; 
+    loop {
+        t += n;
+        n += 1;
+        let mut prime_factors: Vec<i32> = Vec::new();
+        prime_factors = prime_factorizer(t, 2, prime_factors);
+        let mut unique_factors = prime_factors.clone();
+        unique_factors.dedup();
+        let mut div_count = 1;
+        for x in unique_factors.iter() {
+            let mut freq = 0;
+            for y in prime_factors.iter() {
+                if x == y {
+                    freq += 1;
+                }
+            }
+            div_count *= freq + 1;
+        }
+        if div_count > 500  {
+            println!("Problem 12: {}", t); 
+            break;
+        }
+    }
+}
+
+// https://projecteuler.net/problem=13
+fn problem13() {
+
 }
 
 fn main() {
@@ -329,5 +402,6 @@ fn main() {
     problem8();
     problem9();
     problem10();
-//    problem11();
+    problem11();
+    problem12();
 }
